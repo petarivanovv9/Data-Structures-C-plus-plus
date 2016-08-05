a232f57d635b3339706d58c0845dfd2c9b3bee67
Petar Ivanov <pepincho95@gmail.com>
2016-08-05 14:55:41 +0300

    Add initial dynamic-Queue-with-LinkedNodes project.

---
+++ dynamic-Stack-with-LinkedNodes/dynamic-Stack-with-LinkedNodes/LinkedStack.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <iostream>
+
+
+template <typename TYPE>
+class LinkedStack {
+
+	class Node {
+	public:
+		TYPE _data;
+		Node* _pNext;
+		Node(TYPE, Node* = NULL);
+	};
+
+public:
+	LinkedStack();
+	~LinkedStack();
+	LinkedStack(LinkedStack<TYPE> const & other);
+	LinkedStack& operator=(LinkedStack<TYPE> const & other);
+
+public:
+	bool push(TYPE);
+	bool pop();
+	TYPE peek() const;
+
+	void removeAll();
+
+	size_t getAllocatedSize() const;
+	size_t getSize() const;
+
+	bool isEmpty() const;
+
+private:
+	void init();
+	void destroy();
+	void copyFrom(LinkedStack<TYPE> const &);
+
+private:
+	Node* _pTop;
+	size_t _used;
+};
+
